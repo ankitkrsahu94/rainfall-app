da788b1dfb5d0ee4ec7c3e7a7ac7afddd9601f31
Ankit <ankitkrsahu94@gmail.com>
2019-02-13 12:00:54 +0530

    esri error removed

--- src/app/waterdataonline/gis/gis.component.ts
+++ src/app/waterdataonline/gis/gis.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import * as maptalks from 'maptalks';
-import { esri } from 'maptalks.esri';
+// import { esri } from 'maptalks.esri';
 import { ConstantsModule } from '../../constants.module';
 
 @Component({
@@ -17,9 +17,9 @@ export class GisComponent implements OnInit {
     this.parentLocName = this.route.snapshot.paramMap.get('parentLocName');
     if(!this.parentLocName)
     this.router.navigate(["/waterdataonline/gis", this.constants.DEFAULT_COUNTRY]);
-    // this.drawMap();
-    this.drawFeatureLayer();
-    // this.addWMSLayers();
+    this.drawMap();
+    // this.drawFeatureLayer();
+    this.addWMSLayers();
   }
 
   drawMap(): void{
@@ -98,7 +98,7 @@ export class GisComponent implements OnInit {
     let districtLayer:maptalks.WMSTileLayer = new maptalks.WMSTileLayer('District', {
       'urlTemplate' : 'http://portal.tamcnhp.com/server/services/ewis/eswdes_service3/MapServer/WMSServer?',
       'crs' : 'EPSG:4326',
-      'layers' : 'CDATA:0',
+      'layers' : 'eswdes_service3:subbasins',
       'styles' : 'default',
       // 'version' : '1.1.1',
       'format': 'image/png',
@@ -111,52 +111,52 @@ export class GisComponent implements OnInit {
   
   }
 
-  drawFeatureLayer(): void{
-    this.map = new maptalks.Map('map', {
-      //center: [-122.23,37.75],
-      center:[ 109.22450763502036,34.367512417065313],
-      zoom: 12,
-      attribution: {
-          content: '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/">CARTO</a>'
-      },
-      baseLayer: new maptalks.TileLayer('base', {
-          urlTemplate: 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
-          subdomains: ['a', 'b', 'c', 'd']
-      })
-    });
-    var flayer = new maptalks.esri.FeatureLayer('flayer','http://117.36.75.134:6080/arcgis/rest/services/LT/WBJ/FeatureServer/1',{
-        symbol:{
-            // markerFile:'images/apple.png',
-            'markerType': 'path',
-            'markerPath': 'M8 23l0 0 0 0 0 0 0 0 0 0c-4,-5 -8,-10 -8,-14 0,-5 4,-9 8,-9l0 0 0 0c4,0 8,4 8,9 0,4 -4,9 -8,14z M3,9 a5,5 0,1,0,0,-0.9Z',
-            lineColor: 'red'
-        }
-    });
-    flayer.addTo(this.map);
-    flayer.on('loadend', function (e) {
-        var geometries = e.geometries;
-        console.log(geometries.length);
-    });
-    var features =[{
-        "geometry" : {"x" : 114.25, "y" : 33.80},      
-        "attributes" : {
-          "id" : "1",
-          "name" : "Joe Smith"
-        }
-      },
-      {
-        "geometry" : { "x" : 114.27, "y" : 34.086 },      
-        "attributes" : {
-          "id" : "2",
-          "name" : "John Doe"
-        }
-      }
-    ];
-    //增加要素
-    flayer.addFeatures(features,function(res){
-        console.log(res);
-    });
-  }
+  // drawFeatureLayer(): void{
+  //   this.map = new maptalks.Map('map', {
+  //     //center: [-122.23,37.75],
+  //     center:[ 109.22450763502036,34.367512417065313],
+  //     zoom: 12,
+  //     attribution: {
+  //         content: '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/">CARTO</a>'
+  //     },
+  //     baseLayer: new maptalks.TileLayer('base', {
+  //         urlTemplate: 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+  //         subdomains: ['a', 'b', 'c', 'd']
+  //     })
+  //   });
+  //   var flayer = new maptalks.esri.FeatureLayer('flayer','http://117.36.75.134:6080/arcgis/rest/services/LT/WBJ/FeatureServer/1',{
+  //       symbol:{
+  //           // markerFile:'images/apple.png',
+  //           'markerType': 'path',
+  //           'markerPath': 'M8 23l0 0 0 0 0 0 0 0 0 0c-4,-5 -8,-10 -8,-14 0,-5 4,-9 8,-9l0 0 0 0c4,0 8,4 8,9 0,4 -4,9 -8,14z M3,9 a5,5 0,1,0,0,-0.9Z',
+  //           lineColor: 'red'
+  //       }
+  //   });
+  //   flayer.addTo(this.map);
+  //   flayer.on('loadend', function (e) {
+  //       var geometries = e.geometries;
+  //       console.log(geometries.length);
+  //   });
+  //   var features =[{
+  //       "geometry" : {"x" : 114.25, "y" : 33.80},      
+  //       "attributes" : {
+  //         "id" : "1",
+  //         "name" : "Joe Smith"
+  //       }
+  //     },
+  //     {
+  //       "geometry" : { "x" : 114.27, "y" : 34.086 },      
+  //       "attributes" : {
+  //         "id" : "2",
+  //         "name" : "John Doe"
+  //       }
+  //     }
+  //   ];
+  //   //增加要素
+  //   flayer.addFeatures(features,function(res){
+  //       console.log(res);
+  //   });
+  // }
 
 
   // drawArcGISMap(): void{
